3e0ae5e53928ba52a6afd94b0aa87207f670ac8c
Nicolas Dunand <nico@kolya.ch>
2018-09-20 08:06:14 +0200

    Bumped version number for bugfix release

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018092000;
+$plugin->version   = 2018092001;
 $plugin->requires  = 2017050500; // Moodle 3.3
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "2.2 (Build: 2018092000)";
+$plugin->release = "2.2.1 (Build: 2018092001)";
 $plugin->dependencies = array(
     'assignfeedback_editpdf' => 2017051500
 );
